511e4d7ccdfaf41c5a9b06e59dc46a6d447bc04d
Namgal <35800136+GeraldNamgal@users.noreply.github.com>
2019-12-03 17:44:35 -0500

    Saving changes

---
+++ src/com/cscie97/ists/ipfs/ObjectFactory.java
@@ -0,0 +1,17 @@
+package com.cscie97.ists.ipfs;
+
+import com.cscie97.ists.customer.Book;
+
+public class ObjectFactory {
+
+    public static Object getObject(String json) {
+        //if("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
+        //else if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+        
+        if(true) return new Book(null, null, null, null, null);
+        
+        // ...
+        
+        return null;
+    }
+}
